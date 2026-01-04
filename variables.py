daff3116c893089288792a6858faef3c765feb0a
delhassali5620-lang <d.elhassali5620@uca.ac.ma>
2026-01-04 20:03:04 +0100

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,13 @@
+# variables.py
+
+# Déclaration de variables
+entier = 42              # int
+flottant = 3.14          # float
+texte = "Python"         # str
+vrai_ou_faux = True      # bool
+
+# Affichage des variables et de leur type
+print(entier, "→", type(entier))
+print(flottant, "→", type(flottant))
+print(texte, "→", type(texte))
+print(vrai_ou_faux, "→", type(vrai_ou_faux))
